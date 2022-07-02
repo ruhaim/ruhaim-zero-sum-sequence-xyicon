e38d5c907da39b059d9ee93a6712843b6c51e5cd
ruhaim <ruhaim@gmail.com>
2022-07-02 12:38:28 +0530

    2 files modified, 4 added

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
 // run `node index.js` in the terminal
+const fs = require('fs');
+const readline = require('readline');
+
+const rl = readline.createInterface({
+    input: fs.createReadStream('sample.txt'),
+    crlfDelay: Infinity
+});
+
+rl.on('line', (line) => {
+    console.log(`Line from file: ${line}`);
+});
 
 console.log(`Hello Node.js v${process.versions.node}!`);
+
+
