bfaa7543b7b885899b3c8e0af22b373b8b37cef7
ruhaim <ruhaim@gmail.com>
2022-07-02 15:55:50 +0530

    3 files modified, 1 deleted

--- index.js
+++ index.js
@@ -1,16 +1,32 @@
-// run `node index.js` in the terminal
-const fs = require('fs');
-const readline = require('readline');
+const lineByLine = require('n-readlines');
 
-const rl = readline.createInterface({
-    input: fs.createReadStream('sample.txt'),
-    crlfDelay: Infinity
-});
+const liner = new lineByLine('./example.in');
 
-rl.on('line', (line) => {
-    console.log(`Line from file: ${line}`);
-});
+const totalCases = parseInt(liner.next().toString());
+for (let i = 0; i < totalCases; i++) {
+  const numsPerArr = parseInt(liner.next().toString());
+  const input = liner
+    .next()
+    .toString()
+    .split(' ')
+    .map((s) => parseInt(s.trim()));
+  console.log(hasZeroSum(input));
+}
 
-console.log(`Hello Node.js v${process.versions.node}!`);
+function hasZeroSum(arr) {
+  const prefixSum = Array(arr.length + 2).fill(0);
+  const prevSeenMap = { 0: true };
 
+  const result = arr.some((num, idx) => {
+    prefixSum[idx + 1] += prefixSum[idx] + num;
+    const isPrevSeen = prevSeenMap[prefixSum[idx + 1]];
 
+    if (isPrevSeen) {
+      return true;
+    } else {
+      prevSeenMap[prefixSum[idx + 1]] = true;
+    }
+  });
+
+  return result ? 'yes' : 'no';
+}
